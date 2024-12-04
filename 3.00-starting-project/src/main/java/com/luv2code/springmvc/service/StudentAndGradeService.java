939bf08785d2bc47906b7fcd5fc76aa977882f37
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-12-04 11:19:47 -0300

    tests and spring-boot

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,5 +20,24 @@ public class StudentAndGradeService {
       CollegeStudent student = new CollegeStudent(firstName, lastName, emailAddress);
       student.setId(0);
       studentDao.save(student);
+  }
+
+  public boolean checkIfStudentIsNull(int id){
+      Optional<CollegeStudent> student = studentDao.findById(id);
+      if (student.isPresent()) {
+          return true;
+      }
+      return false;
+  }
+
+  public void deleteStudent(int id){
+      if (checkIfStudentIsNull(id)) {
+          studentDao.deleteById(id);
+      }
+  }
+
+  public Iterable<CollegeStudent> getGradebook() {
+      Iterable<CollegeStudent> collegeStudents = studentDao.findAll();
+      return collegeStudents;
   }
 }
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -3,16 +3,28 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
 
     @Autowired
     private StudentAndGradeService studentService;
@@ -20,13 +32,65 @@ public class StudentAndGradeServiceTest {
      @Autowired
     private StudentDao studentDao;
 
+     @BeforeEach
+     public void setupDatabase(){
+          jdbc.execute("insert into student(id, firstName, lastName, email_address) " +
+                   "values (1, 'Eric', 'Roby', 'eric.roby@luv2code_school.com')");
+     }
+
     @Test
     public void createStudentService(){
 
-        studentService.createStudent("Chad", "Darby", "chad.darby@luv2code_school.com");
+        studentService.createStudent("Chad", "Darby",
+                "chad.darby@luv2code_school.com");
 
-        CollegeStudent student = studentDao.findByEmailAddress("chad.darby@luv2code_school.com");
+        CollegeStudent student = studentDao.
+                findByEmailAddress("chad.darby@luv2code_school.com");
 
-        assertEquals("chad.darby@luv2code_school.com", student.getEmailAddress(), "find by email");
+        assertEquals("chad.darby@luv2code_school.com",
+                student.getEmailAddress(), "find by email");
+    }
+
+    @Test
+    public void isStudentNullCheck(){
+
+        assertTrue(studentService.checkIfStudentIsNull(1));
+
+        assertFalse(studentService.checkIfStudentIsNull(0));
+    }
+
+    @Test
+    public void deleteStudentService(){
+
+        Optional<CollegeStudent> deletedCollegeStudent = studentDao.findById(1);
+
+        assertTrue(deletedCollegeStudent.isPresent(), "Return True");
+
+         studentService.deleteStudent(1);
+
+         deletedCollegeStudent = studentDao.findById(1);
+
+         assertFalse(deletedCollegeStudent.isPresent(), "Return False");
+     }
+
+
+     @Sql("/insertData.sql")
+     @Test
+     public void getGradebookService(){
+         Iterable<CollegeStudent> iterableCollegeStudents = studentService.getGradebook();
+
+         List<CollegeStudent> collegeStudents = new ArrayList<>();
+
+         for (CollegeStudent collegeStudent : iterableCollegeStudents) {
+             collegeStudents.add(collegeStudent);
+         }
+
+         assertEquals(5, collegeStudents.size());
+     }
+
+
+    @AfterEach
+    public void setupAfterTransaction(){
+         jdbc.execute("DELETE FROM student");
     }
 }
