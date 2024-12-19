64ae2f72e8431dd5775ccb0cd39c8eb79624f294
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-12-19 10:44:54 -0300

    Delete 3.01-starting-project-solutions directory

--- 3.01-starting-project-solutions/3.01-13-solution-create-grade-service-passed-for-math-grades/src/main/java/com/luv2code/springmvc/repository/MathGradesDao.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springmvc.repository;
-
-import com.luv2code.springmvc.models.MathGrade;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MathGradesDao extends CrudRepository<MathGrade, Integer> {
-
-    public Iterable<MathGrade> findGradeByStudentId(int id);
-}
